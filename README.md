98cb456053421a4e65f3103850620c97155a7c42
Khaled Khalil <khaled.m.hassan.94@gmail.com>
2021-02-18 14:31:32 +0200

    README file updated and styling

--- README.md
+++ README.md
@@ -1,7 +1,27 @@
-# codingChallenge
-This is a simple repo shows the latest (according to running date) most starred repos on Github.
+# Front-end Coding Challenge
 
-To start:
-clone the repo.
-run "npm install"
-then "npm start"
+## About The Project
+* This task is to implement a small webapp that shows in a descending order the most starred repos on GitHub in the past 30 days from when you're running it.
+* JSON data was fetched directely from GitHub API as shown below in Details
+
+## Details:
+* Repos are shown 10 rows per page and after page #10 you should press on *** Get 100 Repos *** button to get the next page in Github API.
+
+* Each row consists of:
+   * Repository name
+   * Repository description 
+   * Number of stars for the repo. 
+   * Number of issues for the repo.
+   * Username and avatar of the owner. 
+
+* GitHub Api:
+`https://api.github.com/search/repositories?q=created:${todayDate}&sort=stars&order=desc&page=${pageNumber}`
+* Where *** todayDate & pageNumber *** are 2 variables that change according to date and page number 
+
+## Technologies used 
+*** React ***
+
+#### To start:
+* clone the repo to your local PC
+* run `npm install`
+* then `npm start`
--- src/App.js
+++ src/App.js
@@ -4,118 +4,123 @@ import ReactPaginate from "react-paginate";
 import './App.css';
 
 function App() {
-  const [dateFrom30DaysAgo, setDateFrom30DaysAgo] = useState("");
-  const [Repos, setRepos] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [currentPage, setCurrentPage] = useState(0);
-  const [apiPageNumber, setApiPageNumber] = useState(1);
+    const [dateFrom30DaysAgo, setDateFrom30DaysAgo] = useState("");
+    const [Repos, setRepos] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [currentPage, setCurrentPage] = useState(0);
+    const [apiPageNumber, setApiPageNumber] = useState(1);
 
-  const setDate = () => {
-    let today = new Date(),
-      date = today.getFullYear()
-        + '-'
-        + (today.getMonth().toLocaleString('en-US', { minimumIntegerDigits: 2, useGrouping: false }))
-        + '-'
-        + today.getDate()
-      ;
-    setDateFrom30DaysAgo(date);
-  }
+    const setDate = () => {
+        let today = new Date(),
+            date = today.getFullYear()
+                + '-'
+                + (today.getMonth().toLocaleString('en-US', { minimumIntegerDigits: 2, useGrouping: false }))
+                + '-'
+                + today.getDate()
+            ;
+        setDateFrom30DaysAgo(date);
+    }
 
-  useEffect(() => {
-    setDate();
-  }, []);
+    useEffect(() => {
+        setDate();
+    }, []);
 
-  const githubRepos = () => {
-    setLoading(true);
-    Axios
-      .get(`https://api.github.com/search/repositories?q=created:${dateFrom30DaysAgo}&sort=stars&order=desc&page=${apiPageNumber}&per_page=100`)
-      .then((res) => {
-        setLoading(false);
-        setRepos(res.data.items);
-        console.log(dateFrom30DaysAgo);
-      })
-      .catch(() => {
-        alert("Only the first 1000 search results are available")
-      })
-  };
+    const githubRepos = () => {
+        setLoading(true);
+        Axios
+            .get(`https://api.github.com/search/repositories?q=created:${dateFrom30DaysAgo}&sort=stars&order=desc&page=${apiPageNumber}&per_page=100`)
+            .then((res) => {
+                setLoading(false);
+                setRepos(res.data.items);
+                console.log(dateFrom30DaysAgo);
+            })
+            .catch(() => {
+                alert("Error 402: Only the first 1000 search results are available.")
+            })
+    };
 
-  const loadMoreGitHubRepos = () => {
-    setApiPageNumber(apiPageNumber + 1);
-    githubRepos();
-  }
+    const loadMoreGitHubRepos = () => {
+        setApiPageNumber(apiPageNumber + 1);
+        githubRepos();
+    }
 
-  const renderRepo = (repo) => {
-    const ownerAvatar = repo.owner.avatar_url;
-    const stars = repo.stargazers_count;
-    const issues = repo.open_issues_count;
-    const createdAt = repo.created_at;
+    const renderRepo = (repo) => {
+        const ownerAvatar = repo.owner.avatar_url;
+        const ownerName = repo.owner.login;
+        const stars = repo.stargazers_count;
+        const issues = repo.open_issues_count;
+        const createdAt = repo.created_at;
+        return (
+            <div className="row" key={repo.id}>
+                <img src={ownerAvatar} alt="Owner's Avatar" width="150" height="150" />
+                <div className="repo-name">
+                    <h2>
+                        Owner: {ownerName} <br></br>
+                        Name: <a href={repo.html_url}>{repo.name}</a>
+                    </h2>
+                    <p>
+                        {repo.description ? repo.description : "Sorry, No Description Availabe!"}
+                    </p>
+                    <p>
+                        <span>Stars: {stars}</span>
+                        <span>Issues: {issues}</span>
+                    </p>
+                    <p>
+                        Created: {createdAt.split('T')[0]}
+                    </p>
+                </div>
+            </div>
+        );
+    };
+
+
+    // Pagination
+    const PER_PAGE = 10; //no. of items/page
+    const offset = currentPage * PER_PAGE;
+    const currentPageRepos = Repos
+        .slice(offset, offset + PER_PAGE)
+        .map(renderRepo);
+    const pageCount = Math.ceil(Repos.length / PER_PAGE);
+
+    function handlePageClick({ selected: selectedPage }) {
+        setCurrentPage(selectedPage);
+    }
+    //end of pagination
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        loadMoreGitHubRepos();
+    };
+
+
     return (
-      <div className="row" key={repo.id}>
-
-        <img src={ownerAvatar} alt="Owner's Avatar" width="150" height="150" />
-        <div className="repo-name">
-          <h2>
-            <a href={repo.html_url}>{repo.name}</a>
-          </h2>
-          <p>{repo.description}</p>
-          <p><span>Stars: {stars}</span> <span>Issues: {issues}</span> Created: {createdAt}</p>
+        <div className="page">
+            <div className="landing-page-container">
+                <ReactPaginate
+                    previousLabel={"← Previous"}
+                    nextLabel={"Next →"}
+                    pageCount={pageCount}
+                    onPageChange={handlePageClick}
+                    containerClassName={"pagination"}
+                    previousLinkClassName={"pagination__link"}
+                    nextLinkClassName={"pagination__link"}
+                    disabledClassName={"pagination__link--disabled"}
+                    activeClassName={"pagination__link--active"}
+                />
+                <div>
+                    <button onClick={handleSubmit}>
+                        {loading ? "Getting Repos..." : "Get 100 Repos"}
+                    </button>
+                    <div>
+                    </div>
+                    <div className="results-container">
+                        {currentPageRepos}
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
     );
-  };
-
-
-  // Pagination
-  const PER_PAGE = 10; //no. of items/page
-  const offset = currentPage * PER_PAGE;
-  const currentPageRepos = Repos
-    .slice(offset, offset + PER_PAGE)
-    .map(renderRepo);
-  const pageCount = Math.ceil(Repos.length / PER_PAGE);
-
-  function handlePageClick({ selected: selectedPage }) {
-    setCurrentPage(selectedPage);
-  }
-  //end of pagination
-
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    loadMoreGitHubRepos();
-  };
-
-
-  return (
-    <div className="page">
-      <div className="landing-page-container">
-        <ReactPaginate
-          previousLabel={"← Previous"}
-          nextLabel={"Next →"}
-          pageCount={pageCount}
-          onPageChange={handlePageClick}
-          containerClassName={"pagination"}
-          previousLinkClassName={"pagination__link"}
-          nextLinkClassName={"pagination__link"}
-          disabledClassName={"pagination__link--disabled"}
-          activeClassName={"pagination__link--active"}
-        />
-        <div>
-          <button onClick={handleSubmit}>
-            {loading ? "Getting Repos..." : "Get 100 Repos"}
-          </button>
-          <div>
-
-          </div>
-          <div className="results-container">
-            {currentPageRepos}
-          </div>
-          {/* <button className="more_repos" onClick={loadMoreGitHubRepos}>
-            Load the next 100 Repos
-          </button> */}
-        </div>
-      </div>
-    </div>
-  );
 }
 
 export default App;
